b7a5ba125b858fea6a5f99cb9bea70f062662769
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2024-04-30 09:13:11 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { useLocation } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import RouterController from "./RouterController";
 
-function App() {
+
+const App = () => {
+  const { pathname } = useLocation();
   return (
     <>
+      {!pathname.includes("/admin") && !pathname.includes("/login") && <Nav/>}
+      <RouterController/>
+      {!pathname.includes("/admin") && !pathname.includes("/login") && <Footer/>}
+      </>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
